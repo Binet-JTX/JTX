e2b7351144819421f654a2ccbb7ab420b300eefe
fandejuni <t.dardinier@gmail.com>
2016-12-22 17:38:11 +0100

    Refactored pagination, templates, and changed category

--- videos/views.py
+++ videos/views.py
@@ -4,8 +4,11 @@ from django.core.urlresolvers import reverse
 
 from .models import *
 
-n_page = 4
+n_page = 20
 n_index = 5
+
+def id(x):
+    return x
 
 def index(request):
     projs = Proj.objects.filter(category__public=True)
@@ -32,30 +35,37 @@ def categories(request):
     }
     return render(request, 'categories.html', context)
 
-def category(request, category_id):
+def pagination(request, template, context, elements, page, adress, f = id):
+    page = int(page)
+    nb_page = ((elements.count() - 1) // n_page) + 1
+    elements = elements.all()[(page - 1) * n_page:page * n_page]
+    context['pages'] = range(1, nb_page + 1)
+    context['page'] = page
+    elements = map(f, elements)
+    context['elements'] = elements
+    context['adress'] = 'videos:' + adress
+    return render(request, template, context)
+
+def category(request, category_id, page=1):
     cat = get_object_or_404(Category, pk=category_id)
     if cat.public or request.user.is_authenticated:
         projs = Proj.objects.filter(category=cat)
         context = {
-            'category': cat,
-            'projs': projs,
+            'titre': cat.titre,
+            'id': category_id,
         }
-        return render(request, 'category.html', context)
-    return index(request)
+        return pagination(request, 'projs.html', context, projs, page, 'category')
+    else:
+        return index(request)
 
 def projs(request, page=1):
     projs = Proj.objects.filter(category__public=True)
     if (request.user.is_authenticated):
         projs = Proj.objects
-    page = int(page)
-    nb_page = ((projs.count() - 1) // n_page) + 1
-    projs = projs.all()[(page - 1) * n_page:page * n_page]
     context = {
-        'page': page,
-        'pages': range(1, nb_page + 1),
-        'projs': projs,
+        'titre': 'Toutes les projs',
     }
-    return render(request, 'projs.html', context)
+    return pagination(request, 'projs.html', context, projs, page, 'projs')
 
 def proj(request, proj_id):
     proj = get_object_or_404(Proj, pk=proj_id)
@@ -72,15 +82,10 @@ def favorites(request, page=1):
     if (request.user.is_authenticated):
         user = request.user
         favorites = Favorite.objects.filter(user = user)
-        page = int(page)
-        nb_page = ((favorites.count() - 1) // n_page) + 1
-        favorites = favorites.all()[(page - 1) * n_page:page * n_page]
         context = {
-            'page': page,
-            'pages': range(1, nb_page + 1),
-            'favorites': favorites,
+            'titre': 'Vidéos aimées',
         }
-        return render(request, 'favorites.html', context)
+        return pagination(request, 'videos.html', context, favorites, page, 'favorites', lambda x: x.video)
     else:
         return index(request)
 
@@ -88,15 +93,10 @@ def videos(request, page=1):
     videos = Video.objects.filter(public=True)
     if (request.user.is_authenticated):
         videos = Video.objects
-    page = int(page)
-    nb_page = ((videos.count() - 1) // n_page) + 1
-    videos = videos.all()[(page - 1) * n_page:page * n_page]
     context = {
-        'page': page,
-        'pages': range(1, nb_page + 1),
-        'videos': videos,
+        'titre': 'Toutes les vidéos',
     }
-    return render(request, 'videos.html', context)
+    return pagination(request, 'videos.html', context, videos, page, 'videos')
 
 def video(request, video_id):
     video = get_object_or_404(Video, pk=video_id)
@@ -105,7 +105,7 @@ def video(request, video_id):
         video.save()
         n = Favorite.objects.filter(video = video).count()
         favorite = False
-        if (request.user.is_authenticated):
+        if request.user.is_authenticated:
             user = request.user
             favorite = Favorite.objects.filter(user = user, video = video).exists()
         context = {
@@ -123,10 +123,17 @@ def tags(request):
     }
     return render(request, 'tags.html', context)
 
-def tag(request, tag_id):
-    #TODO : À modifier pour public
+def tag(request, tag_id, page=1):
     tag = get_object_or_404(Tag, pk=tag_id)
-    return render(request, 'tag.html', {'tag': tag})
+    videos = tag.relation_tag_set.filter(video__public=True)
+    if request.user.is_authenticated:
+        videos = tag.relation_tag_set
+    context = {
+        'titre': tag.titre,
+        'titre_tag': True,
+        'id': tag_id,
+    }
+    return pagination(request, 'videos.html', context, videos, page, 'tag', lambda x: x.video)
 
 def remove_favorite(request, video_id):
     if request.user.is_authenticated:
