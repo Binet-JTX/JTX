30619e05f09e75354518cfeea7e9da81910632c3
fandejuni <t.dardinier@gmail.com>
2017-04-15 21:33:58 +0200

    UTF8

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
